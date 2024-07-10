f5df44433cabb47a5eb39660b251f1d8e39163cd
Rogerio-17 <rogeriojmf10@gmail.com>
2024-07-09 22:24:39 -0300

    feat: finaliza API de post de trips e inicia API de confirmação de trips

---
+++ src/routes/confirm-trip.ts
@@ -0,0 +1,56 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { getMailClient } from "../lib/mail";
+import { dayjs } from "../lib/dayjs";
+
+export async function confirmTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/trips/:tripId/confirm",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { tripId } = request.params;
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+        include: {
+          participants: {
+            where: {
+              is_owner: false,
+            },
+          },
+        },
+      });
+
+      if (!trip) {
+        throw new Error("Trip not found.");
+      }
+
+      if (trip.is_confirmed) {
+        return reply.redirect(`http://localhost:3000/trips/${tripId}`);
+      }
+
+      await prisma.trip.update({
+        where: { id: tripId },
+        data: {
+          is_confirmed: true,
+        },
+      });
+      const formattedStartDate = dayjs(trip.starts_at).format("LL");
+      const formattedEndDate = dayjs(trip.ends_at).format("LL");
+
+      const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm/ID_DO_PARTICIPANTE`;
+
+      return { tripId: request.params.tripId };
+    }
+  );
+}
