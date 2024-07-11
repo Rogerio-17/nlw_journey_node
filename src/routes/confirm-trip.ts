4be2fd45c14005ae7804a0e8652f8bbd266d9376
Rogerio-17 <rogeriojmf10@gmail.com>
2024-07-11 08:19:15 -0300

    feat: Finaliza rotas de confirmação de viagem, de participantes, atividades e links

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import { getMailClient } from "../lib/mail";
 import { dayjs } from "../lib/dayjs";
+import nodemailer from "nodemailer";
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -48,9 +49,39 @@ export async function confirmTrip(app: FastifyInstance) {
       const formattedStartDate = dayjs(trip.starts_at).format("LL");
       const formattedEndDate = dayjs(trip.ends_at).format("LL");
 
-      const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm/ID_DO_PARTICIPANTE`;
+      const mail = await getMailClient();
 
-      return { tripId: request.params.tripId };
+      await Promise.all(
+        trip.participants.map(async (participant) => {
+          const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`;
+          const message = await mail.sendMail({
+            from: {
+              name: "Equipe plann.er",
+              address: "teste@teste.com",
+            },
+
+            to: participant.email,
+            subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
+            html: `
+            <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+              <p>Você foi convidado(a) para participar de uma viagem para <strong>${trip.destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+              <p></p>
+              <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
+              <p></p>
+              <p>
+                <a href="${confirmationLink}">Confirmar viagem</a>
+              </p>
+              <p></p>
+              <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+            </div>
+          `.trim(),
+          });
+
+          console.log(nodemailer.getTestMessageUrl(message));
+        })
+      );
+
+      return reply.redirect(`http://localhost:3000/trips/${tripId}`);
     }
   );
 }
