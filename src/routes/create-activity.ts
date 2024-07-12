bcb17d77458e28fdb70f222a550776d015aee019
Rogerio-17 <rogeriojmf10@gmail.com>
2024-07-11 22:53:09 -0300

    feat: Adicionada a validação de errors para retornar informações mais precisas sobre o erro

--- src/routes/create-activity.ts
+++ src/routes/create-activity.ts
@@ -3,6 +3,7 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { dayjs } from "../lib/dayjs";
 import { prisma } from "../lib/prisma";
+import { ClientError } from "../errors/client-error";
 
 export async function createActivity(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -29,15 +30,15 @@ export async function createActivity(app: FastifyInstance) {
       });
 
       if (!trip) {
-        throw new Error("Trip not found");
+        throw new ClientError("Trip not found");
       }
 
       if (dayjs(accurs_at).isBefore(trip.starts_at)) {
-        throw new Error("Invalid activity data");
+        throw new ClientError("Invalid activity data");
       }
 
       if (dayjs(accurs_at).isAfter(trip.ends_at)) {
-        throw new Error("Invalid activity data");
+        throw new ClientError("Invalid activity data");
       }
 
       const activity = await prisma.activity.create({
